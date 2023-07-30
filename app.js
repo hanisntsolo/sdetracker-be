666e896c7992709d38921933adce825421bdb9a9
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2023-07-30 09:35:01 +0000

    user creation logic added

--- app.js
+++ app.js
@@ -36,6 +36,32 @@ async function getUserData() {
 
 getUserData();
 
+async function createUser() {
+  try {
+    const db = client.db('your-database-name'); // Replace 'your-database-name' with your actual database name
+    const usersCollection = db.collection('users');
+
+    // The user document you want to insert
+    const newUser = {
+      username: 'john_doe',
+      email: 'john.doe@example.com',
+      password: 'hashed_password_here', // You should store a hashed password, not the plain password
+      role: 'user', // Optional: Add any other user-related data you need
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+    console.log('User created:', result.insertedId);
+  } catch (err) {
+    console.error('Error creating user:', err);
+  }
+}
+
+// Call the functions to connect to the database and create a user
+connectToDatabase()
+  .then(() => createUser())
+  .catch((err) => console.error('Error:', err));
+
+getUserData();
 
 // Define a simple route
 app.get('/', (req, res) => {
